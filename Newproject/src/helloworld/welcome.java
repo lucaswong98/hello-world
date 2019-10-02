4e27f9f6c4a92daa64e0301f28614e645a14d545
lucaswong98 <kar.hang@hotmail.com>
2019-10-02 10:48:50 +0800

    gta

--- Newproject/src/helloworld/welcome.java
+++ Newproject/src/helloworld/welcome.java
@@ -5,6 +5,9 @@ public class welcome {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("just testing the git thing.");
+		System.out.println("ahh shit");
+		System.out.println("here we go again.");
+		
 	}
 
 }
