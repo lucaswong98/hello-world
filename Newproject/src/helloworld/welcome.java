f29ac0872d072aaba2d4c91a8553eb055514d256
lucaswong98 <kar.hang@hotmail.com>
2019-10-02 10:55:03 +0800

    Revert "gta"
    This reverts commit 4e27f9f6c4a92daa64e0301f28614e645a14d545.

--- Newproject/src/helloworld/welcome.java
+++ Newproject/src/helloworld/welcome.java
@@ -5,9 +5,6 @@ public class welcome {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("just testing the git thing.");
-		System.out.println("ahh shit");
-		System.out.println("here we go again.");
-		
 	}
 
 }
